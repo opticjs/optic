1d0da1c30d8db936ace7ff9e21d8fcfafb991cd3
Alex Lopatin <alex@siftscience.com>
2015-08-03 03:18:33 -0700

    export the filters

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import Query from './core/Query';
 import Resource from './core/Resource';
 import Response from './core/Response';
 import * as Utils from './core/Utils';
+import QueryCache from './filter_sets/QueryCache';
+import QueryCombiner from './filter_sets/QueryCombiner';
+import ResourceLinker from './filter_sets/ResourceLinker';
 
 export default {
   Resource: Resource,
   Query: Query,
   Response: Response,
   HttpAdapter: HttpAdapter,
-  Utils: Utils
+  Utils: Utils,
+  FilterSets: {
+    QueryCache: QueryCache,
+    QueryCombiner: QueryCombiner,
+    ResourceLinker: ResourceLinker
+  }
 };
