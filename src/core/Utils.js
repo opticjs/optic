757054ead30782f41acbfbcc2d6895afc8a1144f
Alex Lopatin <aleksander.lopatin@gmail.com>
2016-01-06 15:49:51 -0600

    I need a nap

--- src/core/Utils.js
+++ src/core/Utils.js
@@ -102,7 +102,7 @@ export function extend(obj, ...extensions) {
 }
 
 export function merge(objects) {
-  return extend.apply(null, objects[0], objects.slice(1));
+  return extend.apply(null, objects);
 }
 
 export function contains(list, val) {
